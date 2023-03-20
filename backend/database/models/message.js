793b0224f633e748798067d853b6c07fc9db7cb1
julandrod <julian.andres.rodriguez@gmail.com>
2023-03-20 00:28:16 -0500

    (backend) <refactor> remove unnecessary comments

--- backend/database/models/message.js
+++ backend/database/models/message.js
@@ -2,13 +2,7 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Message extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate({ User, Car }) {
-      // define association here}
       this.belongsTo(User, { foreignKey: "senderId" });
       this.belongsTo(Car, { foreignKey: "carId" });
     }
