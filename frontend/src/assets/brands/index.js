905a1f303a1ac19c259f9362ddceb184f3fcc944
julandrod <julian.andres.rodriguez@gmail.com>
2023-03-20 00:38:05 -0500

    (frontend) <add> car logos

---
+++ frontend/src/assets/brands/index.js
@@ -0,0 +1,51 @@
+import audi from "./audi-logo.png";
+import bmw from "./bmw-logo.png";
+import byd from "./byd-logo.png";
+import chevrolet from "./chevrolet-logo.png";
+import dacia from "./dacia-logo.png";
+import fiat from "./fiat-logo.png";
+import ford from "./ford-logo.png";
+import gmc from "./gmc-logo.png";
+import honda from "./honda-logo.png";
+import hyundai from "./hyundai-logo.png";
+import jeep from "./jeep-logo.png";
+import kia from "./kia-logo.png";
+import landRover from "./land-rover-logo.png";
+import mazda from "./mazda-logo.png";
+import mercedesBenz from "./mercedes-benz-logo.png";
+import mitsubishi from "./mitsubishi-logo.png";
+import nissan from "./nissan-logo.png";
+import peugeot from "./peugeot-logo.png";
+import renault from "./renault-logo.png";
+import skoda from "./skoda-logo.png";
+import subaru from "./subaru-logo.png";
+import suzuki from "./suzuki-logo.png";
+import toyota from "./toyota-logo.png";
+import volkswagen from "./volkswagen-logo.png";
+
+export {
+  audi,
+  bmw,
+  byd,
+  chevrolet,
+  dacia,
+  fiat,
+  ford,
+  gmc,
+  honda,
+  hyundai,
+  jeep,
+  kia,
+  landRover,
+  mazda,
+  mercedesBenz,
+  mitsubishi,
+  nissan,
+  peugeot,
+  renault,
+  skoda,
+  subaru,
+  suzuki,
+  toyota,
+  volkswagen,
+};
---
+++ frontend/src/assets/types/index.js
@@ -0,0 +1,7 @@
+import fourByFour from "./4x4.svg";
+import coupe from "./coupe.svg";
+import hatchback from "./hatchback.svg";
+import sedan from "./sedan.svg";
+import suv from "./suv.svg";
+
+export { fourByFour, coupe, hatchback, sedan, suv };
