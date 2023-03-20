f16ff5a0313fab93e9f15c1408e8fd62d74378ef
julandrod <julian.andres.rodriguez@gmail.com>
2023-03-20 00:38:53 -0500

    (frontend) <add> dummy data with logos and car brands

---
+++ frontend/src/dummy-brands.js
@@ -0,0 +1,149 @@
+import {
+  audi,
+  bmw,
+  byd,
+  chevrolet,
+  dacia,
+  fiat,
+  ford,
+  gmc,
+  honda,
+  hyundai,
+  jeep,
+  kia,
+  landRover,
+  mazda,
+  mercedesBenz,
+  mitsubishi,
+  nissan,
+  peugeot,
+  renault,
+  skoda,
+  subaru,
+  suzuki,
+  toyota,
+  volkswagen,
+} from "./assets/brands";
+
+export const brands = [
+  {
+    id: 1,
+    logo: audi,
+    brand: "audi",
+  },
+  {
+    id: 2,
+    logo: bmw,
+    brand: "bmw",
+  },
+  {
+    id: 3,
+    logo: byd,
+    brand: "byd",
+  },
+  {
+    id: 4,
+    logo: chevrolet,
+    brand: "chevrolet",
+  },
+  {
+    id: 5,
+    logo: dacia,
+    brand: "dacia",
+  },
+  {
+    id: 6,
+    logo: fiat,
+    brand: "fiat",
+  },
+  {
+    id: 7,
+    logo: ford,
+    brand: "ford",
+  },
+  {
+    id: 8,
+    logo: gmc,
+    brand: "gmc",
+  },
+  {
+    id: 9,
+    logo: honda,
+    brand: "honda",
+  },
+  {
+    id: 10,
+    logo: hyundai,
+    brand: "hyundai",
+  },
+  {
+    id: 11,
+    logo: jeep,
+    brand: "jeep",
+  },
+  {
+    id: 12,
+    logo: kia,
+    brand: "kia",
+  },
+  {
+    id: 13,
+    logo: landRover,
+    brand: "land rover",
+  },
+  {
+    id: 14,
+    logo: mazda,
+    brand: "mazda",
+  },
+  {
+    id: 15,
+    logo: mercedesBenz,
+    brand: "mercedes benz",
+  },
+  {
+    id: 16,
+    logo: mitsubishi,
+    brand: "mitsubishi",
+  },
+  {
+    id: 17,
+    logo: nissan,
+    brand: "nissan",
+  },
+  {
+    id: 18,
+    logo: peugeot,
+    brand: "peugeot",
+  },
+  {
+    id: 19,
+    logo: renault,
+    brand: "renault",
+  },
+  {
+    id: 20,
+    logo: skoda,
+    brand: "skoda",
+  },
+  {
+    id: 21,
+    logo: subaru,
+    brand: "subaru",
+  },
+  {
+    id: 22,
+    logo: suzuki,
+    brand: "suzuki",
+  },
+  {
+    id: 23,
+    logo: toyota,
+    brand: "toyota",
+  },
+  {
+    id: 24,
+    logo: volkswagen,
+    brand: "volkswagen",
+  },
+];
