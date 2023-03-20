3788c041a5d36804c9cd8579871071226028ec6d
julandrod <julian.andres.rodriguez@gmail.com>
2023-03-20 00:43:01 -0500

    (frontend) <add> Home component for the home page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import CarTypes from "../components/CarTypes";
+import Hero from "../components/Hero";
+import CarBrands from "../components/CarBrands";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <CarTypes />
+      <CarBrands />
+    </>
+  );
+};
+
+export default Home;
